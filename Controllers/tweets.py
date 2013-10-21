bcd04340a358bffe6acd0137cd709379e9094c88
Nezare Chafni <nchafni@gmail.com>
2013-10-21 20:53:34 +0000

    enabling a list of stock

--- Controllers/tweets.py
+++ Controllers/tweets.py
@@ -5,17 +5,17 @@ import base64
 import json
 
 
-class tweets(object):
-	def __init__(self,query,accessToken):
-		self.query = query
+class Tweets(object):
+	def __init__(self,q,accessToken):
+		self.q = q
 		self.accessToken = accessToken
-		
-	q = searchQ
-	query = urllib.quote(q)
-	url = "https://api.twitter.com/1.1/search/tweets.json?q="+ query +"&lang=en"
-	rpc = urlfetch.create_rpc()
-	urlfetch.make_fetch_call(rpc,url,headers={"Authorization": "bearer %s" %accessToken})
-	result = rpc.get_result()
-	result = json.loads(result.content)
-	tweets = result
+	def getTweets(self):
+		q = self.q
+		query = urllib.quote(q)
+		url = "https://api.twitter.com/1.1/search/tweets.json?q="+ query +"&lang=en"
+		rpc = urlfetch.create_rpc()
+		urlfetch.make_fetch_call(rpc,url,headers={"Authorization": "bearer %s" %self.accessToken})
+		result = rpc.get_result()
+		tweets = json.loads(result.content)
 	
+		return tweets
